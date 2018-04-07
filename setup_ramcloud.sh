8f8c6440e28aa7a2d43c95df8133e3d42186e534
Henry Qin <root@hq6.me>
2018-04-07 01:17:40 -0700

    Add back from of Makefrag.

--- setup_ramcloud.sh
+++ setup_ramcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script clones RAMCloud and sets up localconfig.py
+# This script clones RAMCloud and sets up DPDK.
 
 SHARED_HOME=/shome
 
@@ -10,6 +10,15 @@ git clone https://github.com/PlatformLab/RAMCloud.git
 cd RAMCloud
 git submodule update --init --recursive
 ln -s ../../hooks/pre-commit .git/hooks/pre-commit
+
+# Generate private makefile configuration
+mkdir private
+cat >>private/MakefragPrivateTop <<EOL
+DEBUG := no
+CCACHE := yes
+LINKER := gold
+DEBUG_OPT := yes
+EOL
 
 # Construct localconfig.py for this cluster setup.
 let num_rcxx=$(geni-get manifest | grep -o "<node " | wc -l)-2

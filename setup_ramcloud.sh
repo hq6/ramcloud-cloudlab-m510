360a9d62df3c07457161d35021901539b4223059
Henry Qin <root@hq6.me>
2018-04-07 00:13:59 -0700

    Eliminate dpdk from RAMCloud setup

--- setup_ramcloud.sh
+++ setup_ramcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script clones RAMCloud and sets up DPDK.
+# This script clones RAMCloud and sets up localconfig.py
 
 SHARED_HOME=/shome
 
@@ -11,23 +11,6 @@ cd RAMCloud
 git submodule update --init --recursive
 ln -s ../../hooks/pre-commit .git/hooks/pre-commit
 
-# Generate private makefile configuration
-mkdir private
-cat >>private/MakefragPrivateTop <<EOL
-DEBUG := no
-CCACHE := yes
-LINKER := gold
-DEBUG_OPT := yes
-GLIBCXX_USE_CXX11_ABI := yes
-DPDK := yes
-DPDK_DIR := dpdk
-DPDK_SHARED := no
-EOL
-
 # Construct localconfig.py for this cluster setup.
 let num_rcxx=$(geni-get manifest | grep -o "<node " | wc -l)-2
 /local/repository/localconfigGen.py $num_rcxx > scripts/localconfig.py
-
-
-## Make dpdk
-MLNX_DPDK=y scripts/dpdkBuild.sh

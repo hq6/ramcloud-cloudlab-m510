3b2344ee91c3636942bfb22bbb357b3fc4b796d0
Henry Qin <root@hq6.me>
2018-04-07 00:16:22 -0700

    Move contents of init_disk_image into setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,33 @@ SHARED_HOME=/shome
 USERS="root `ls /users`"
 
 ################################################################################
+# === Software dependencies that need to be installed. ===
+KERNEL_RELEASE=`uname -r`
+# Common utilities
+apt-get update
+apt-get --assume-yes install mosh vim tmux pdsh tree axel ack-grep htop
+# NFS
+apt-get --assume-yes install nfs-kernel-server nfs-common
+# cpupower, msr-tools (for rdmsr), i7z
+apt-get --assume-yes install linux-tools-common linux-tools-${KERNEL_RELEASE} \
+        cpuset msr-tools i7z
+# Dependencies to build the Linux perf tool
+apt-get --assume-yes install systemtap-sdt-dev libunwind-dev libaudit-dev \
+        libgtk2.0-dev libperl-dev binutils-dev liblzma-dev libiberty-dev
+# Install RAMCloud dependencies
+apt-get --assume-yes install build-essential git-core doxygen libpcre3-dev \
+        protobuf-compiler libprotobuf-dev libcrypto++-dev libevent-dev \
+        libboost-all-dev libgtest-dev libzookeeper-mt-dev zookeeper \
+        libssl-dev default-jdk ccache
+
+################################################################################
+# Remove ulimit on all machines
+cat >> /etc/security/limits.conf <<EOM
+* soft memlock unlimited
+* hard memlock unlimited
+EOM
+
+################################################################################
 # Setup password-less ssh between nodes
 for user in $USERS;
 do
@@ -35,6 +62,7 @@ done
 for user in $USERS; do
     chsh -s `which bash` $user
 done
+
 # Update permissions on backup drive
 chmod 777 /dev/nvme0n1
 

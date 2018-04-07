a9919fd0662b774e657f89a6ca0ec6d535cdbef5
Henry Qin <root@hq6.me>
2018-04-07 15:20:01 -0700

    Generate data network IP addresses instead of control network IP addresses
    This will hopefully cause udp to use the data network instead of the
    control network, resulting in higher throughput.

--- localconfigGen.py
+++ localconfigGen.py
@@ -8,10 +8,10 @@ from sys import argv
 num_rcXX = int(argv[1])
 hosts = []
 for i in range(1, num_rcXX + 1):
-    result = subprocess.Popen(['rsh', 'rc%02d' % i, 'hostname -A; hostname -i'],
+    result = subprocess.Popen(['rsh', 'rc%02d' % i, 'hostname -I'],
         stdout=subprocess.PIPE).communicate()[0]
-    hostname = result.split('.', 1)[0]
-    ipAddr = result.splitlines()[1]
+    hostname = 'rc%02d' % i
+    ipAddr = result.split()[1]
     hosts.append((hostname, ipAddr, i))
 
 print 'hosts = %s' % hosts
